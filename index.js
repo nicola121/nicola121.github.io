bb2c6ee62c5c6f418702c778530ad212320336f0
Binvention <131bairdbk+work@gmail.com>
2022-04-11 21:51:08 -0600

    Test multipage and dynamic html elements

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function openNav() {
-    document.getElementById("sidenav").style.width = "25em";
-  }
+  document.getElementById("sidenav").style.width = "25em";
+}
   
-  function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
+
+function loadSideNav() {
+  element = document.getElementById("sidenav");
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {element.innerHTML = this.responseText;}
+      if (this.status == 404) {element.innerHTML = "Page not found.";}
+    }
   }
+  xhttp.open("GET", "./sidenav.html", true);
+  xhttp.send();
+}
+
+loadSideNav();
