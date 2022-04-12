3bd322eccf0bcc1fe329785dc23058fcdaa1fb38
Binvention <131bairdbk+work@gmail.com>
2022-04-11 22:10:30 -0600

    Finished multipage capability and dynamically load sidemenu and top bar
    Finished verifying multi page capability and set it up so that the side navigation and top bar only need to be edited in one place

--- index.js
+++ index.js
@@ -19,4 +19,19 @@ function loadSideNav() {
   xhttp.send();
 }
 
+function loadTopBar() {
+  element = document.getElementById("topBar");
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {element.innerHTML = this.responseText;}
+      if (this.status == 404) {element.innerHTML = "Page not found.";}
+    }
+  }
+  xhttp.open("GET", "./topBar.html", true);
+  xhttp.send();
+}
+
+
+
 loadSideNav();
